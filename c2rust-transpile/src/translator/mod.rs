awtoau/c2rust

commit 4b720c54c8c50205c2777f73d96c7f157665e464

Map one and zero to true and false in bool expressions

c2rust-transpile/src/translator/mod.rs
@@ -4362,6 +4362,16 @@ impl<'c> Translation<'c> {
                             Box::new(unparen(arg).clone()),
                         )
                     });
+                // Another simplification we can make is mapping {0, 1} => {true, false}
+                } else if let Expr::Lit(ExprLit {
+                    lit: Lit::Int(ref val),
+                    ..
+                }) = *unparen(arg)
+                {
+                    match val.base10_parse() {
+                        Ok(n @ 0) | Ok(n @ 1) => return Ok(mk().lit_expr(mk().bool_lit(n == 1))),
+                        _ => {}
+                    }
                 }
             }
 

commit 3e2bfda52f98f7e45600be10cded32d6fae196e5

transpile: fix missing ExprLit import (broke build since 4b720c54c)
My PR #290 cherry-pick conflict resolution used syn::ExprLit in the
new bool-literal-mapping arm but never added it to the use syn::{...}
import list, so master has not actually compiled since 4b720c54c.
This slipped through because cargo's incremental build did not
recompile c2rust-transpile for the follow-on merges (62c070554,
8d6a2b3da) in this session -- caught by a from-scratch rebuild.

c2rust-transpile/src/translator/mod.rs
@@ -19,7 +19,7 @@ use serde_derive::Serialize;
 use syn::spanned::Spanned as _;
 use syn::{
     AttrStyle, BareVariadic, BinOp, Block, Expr, ExprBinary, ExprBlock, ExprBreak, ExprCast,
-    ExprParen, ExprReturn, ExprUnary, FnArg, ForeignItem, ForeignItemFn, ForeignItemMacro,
+    ExprLit, ExprParen, ExprReturn, ExprUnary, FnArg, ForeignItem, ForeignItemFn, ForeignItemMacro,
     ForeignItemStatic, ForeignItemType, Ident, Item, ItemConst, ItemEnum, ItemExternCrate, ItemFn,
     ItemForeignMod, ItemImpl, ItemMacro, ItemMod, ItemStatic, ItemStruct, ItemTrait,
     ItemTraitAlias, ItemType, ItemUnion, ItemUse, Lit, MacroDelimiter, PathSegment, ReturnType,